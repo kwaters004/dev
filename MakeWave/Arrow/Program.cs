141217bcd26b535661ec09414aa07721080f2e86
Kirk Waters <kwaters004@gmail.com>
2021-03-19 11:33:55 -0400

    Updateing MakeWave with the Arrow program

---
+++ MakeWave/Arrow/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+
+namespace Arrow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int numSpaces = 0;
+
+            
+
+            string preSpaces = new String(' ', numSpaces);
+
+
+            /*
+             *   _____________________|\
+             *  |                       \
+             *  |                        \
+             *  |                        /
+             *  |_____________________  /
+             *                        |/
+             * 
+             */
+
+            string row1 = @" _____________________|\  ";
+            string row2 = @"|                       \ ";
+            string row3 = @"|                        \";
+            string row4 = @"|                        /";
+            string row5 = @"|_____________________  / ";
+            string row6 = @"                      |/  ";
+
+
+            bool forward = true;
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                Console.WriteLine($"{ preSpaces}{ row1}");
+                Console.WriteLine($"{ preSpaces}{ row2}");
+                Console.WriteLine($"{ preSpaces}{ row3}");
+                Console.WriteLine($"{ preSpaces}{ row4}");
+                Console.WriteLine($"{ preSpaces}{ row5}");
+                Console.WriteLine($"{ preSpaces}{ row6}");
+
+
+
+
+                if (forward && i % 20 == 0)
+                {
+                    forward = false;
+                }
+                else if (!forward && i % 20 == 0)
+                {
+                    forward = true;
+                }
+
+                if (forward || numSpaces == 0)
+
+                {
+                    numSpaces++;
+                }
+                else
+                {
+                    numSpaces--;
+                }
+
+                preSpaces = new String(' ', numSpaces);
+
+                Thread.Sleep(10);
+                Console.Clear();
+
+            }
+
+        }
+    }
+}
---
+++ Week2Day2/Week2Day2/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace Week2Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num = 10;
+            //int twice = num % 3;
+            //Console.WriteLine(twice);
+
+            //string first = "George";
+            //string last = "Washington";
+            //last = last.ToUpper();
+
+            //string last = Console.ReadLine().ToUpper();
+            //Console.WriteLine(last);
+
+            // This works
+            // string full = first + " " + last;
+
+            // but this is better
+            //string full = $"Hello {first.ToUpper()} {last} {num + 3}";
+
+            //.ToUpper();
+
+            //Console.WriteLine(full);
+
+            //bool done = false;
+            //Console.WriteLine(done);
+
+
+            //int favorite = 30;
+
+            //while (favorite < 40)
+            //{
+            //    Console.WriteLine(favorite);
+            //    favorite++;
+            //}
+
+            // This string has a length of 0
+            //string myName = "";
+            // This string doesn't have a length
+            //string myName;
+
+            //myName = "Kirk";
+            //Console.WriteLine(myName);
+
+            /* This is not an efficent way to do this
+            
+            int Student1 = 95;
+            int Student2 = 80;
+            int Student3 = 90;
+            int Student4 = 100;
+            int Student5 = 85;
+
+            Console.WriteLine(Student1);
+            Console.WriteLine(Student2);
+            Console.WriteLine(Student3);
+            Console.WriteLine(Student4);
+            Console.WriteLine(Student5); */
+
+            string[] StudentNames = { "Fred", "Julie", "Sam", "Sarah", "Jane" };
+            int[] Students = { 95, 80, 90, 100, 85 };
+
+            //Console.WriteLine(Students);
+            //Console.WriteLine(Students.Length);
+
+            int smarty = 0;
+            int highestNum = 0;
+
+            for (int i = 0; i < Students.Length; i++)
+            {
+
+                if (i == 0)
+                {
+                    highestNum = Students[i];
+                }
+                else if (Students[i] > highestNum)
+                {
+                    highestNum = Students[i];
+                    smarty = i;
+                }
+
+                Console.WriteLine($"{StudentNames[i]} has a grade of {Students[i]}");
+                //Console.WriteLine(smarty);
+            }
+            Console.WriteLine($"\nThe smartest student is {StudentNames[smarty]}.");
+            Console.WriteLine("What a nerd...\n");
+
+            int upper = Students.GetUpperBound(0);
+            Console.WriteLine($"The upper bound of the Students array is {upper}");
+
+            /*
+            int q = 0;
+            q = q + 1;
+            q++;
+            q += 2;
+            q = q + 2;
+
+            q = q - 1;
+            */
+
+            //int q = 0;
+            // the post increment will still change the q variable, but it wont do it when setting the variable
+            // put the increment on it's own line if you need to use it.
+            //int r = q++;
+            //int r = q += 1;
+            //r = q;
+
+            //Console.WriteLine(r);
+            // this is a post increment
+            //Console.WriteLine(q++);
+            //Console.WriteLine(q);
+            //q++;
+
+            // this is a pre increment
+            //++q;
+
+
+
+            
+        }
+    }
+}
